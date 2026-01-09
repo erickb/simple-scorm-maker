fad43c39bae9ef19950ed051cc086981dab19fb7
Eric Kemp-Benedict <ekempbenedict@gmail.com>
2026-01-09 10:41:41 +0000

    Allowing for multiple project outputs

---
+++ notes5120-scorm.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+py-venv/bin/python -m scorm_maker.cli --input ../SOEE5120M-ecol-macro/output --output ./output --config ./notes5120-scorm_config.yaml
--- scorm.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/bash
-py-venv/bin/python -m scorm_maker.cli --input ../SOEE5120M-ecol-macro/output --output ./output
