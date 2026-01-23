b665e51be691220eba9a9ceee6271864050a4460
Eric Kemp-Benedict <ekempbenedict@gmail.com>
2026-01-23 09:19:43 +0000

    Update exercises5120-scorm.sh

--- exercises5120-scorm.sh
+++ exercises5120-scorm.sh
@@ -1,2 +1,3 @@
 #! /usr/bin/bash
-py-venv/bin/python -m scorm_maker.cli --input ../SOEE5120M-problem-sets/output --output ./output --config ./exercises5120-scorm_config.yaml
+py-venv/bin/python -m scorm_maker.cli --input ../SOEE5120M-problem-sets/output --output ./output --config ./exercises-qns-5120-scorm_config.yaml
+py-venv/bin/python -m scorm_maker.cli --input ../SOEE5120M-problem-sets/output --output ./output --config ./exercises-ans-5120-scorm_config.yaml
